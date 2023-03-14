fee9bcbefa188c1bba3065a626d6652e9d21194d
derek-perry <dp@dp.design>
2023-03-14 02:23:37 -0400

    Adjust Links to Use NEXT_PUBLIC Variables
    The environment variables (NEXT_PUBLIC_SITE_URL and SITE_URL) used for links should be public anyway. The OR logic for adding the trailing content was adjusted to actually include that content with the environment variables.

--- pages/500.tsx
+++ pages/500.tsx
@@ -7,7 +7,7 @@ const error500Page: NextPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Error 500: Internal Server Error</title>
-        <link rel="icon" href={process.env.SITE_URL || '.' + '/favicon.ico'} />
+        <link rel="icon" href={process.env.NEXT_PUBLIC_SITE_URL + '/favicon.ico' || './favicon.ico'} />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
@@ -27,7 +27,7 @@ const error500Page: NextPage = () => {
           className="flex items-center justify-center gap-2"
         >
           <a className="hover:text-blue-600" href="https://github.com/derek-perry/next-tailwind-starter" target="_blank" rel="noopener noreferrer">Next+Tailwind Starter</a> - <a className="hover:text-green-600" href="https://dp.design" target="_blank" rel="noopener noreferrer">Derek Perry</a>{' '}
-          <Image src={process.env.SITE_URL || '.' + '/dp.svg'} alt="Derek Perry dp.design Logo" width={50} height={50} />
+          <Image src={process.env.NEXT_PUBLIC_SITE_URL + '/dp.svg' || './dp.svg'} alt="Derek Perry dp.design Logo" width={50} height={50} />
         </div>
       </footer>
     </div>
