e8016ea61f300b620f43d89b659464f7448889dc
DP <dp@dp.design>
2023-09-06 14:47:46 -0400

    Improve robots.txt Generation
    The robots.txt now disallows robots from access any irrelevant nextjs files. (as per reddit.com/r/nextjs/comments/u0soy3/update_your_robotstxt_to_help_google_index_your)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://nexttailwindstarter.netlify.app',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://nexttailwindstarter.netlify.app',
   changefreq: 'weekly',
   generateRobotsTxt: true,
   autoLastmod: true,
@@ -11,6 +11,13 @@ module.exports = {
       {
         userAgent: '*',
         allow: '/',
+        disallow: [
+          '/*.json$',
+          '/*_buildManifest.js$',
+          '/*_middlewareManifest.js$',
+          '/*_ssgManifest.js$',
+          '/*.js$'
+        ]
       }
     ]
   }
